83dcab285cd35506482044ba28822024fb23db94
Md Nurul Hassan <mailme.nurul@gmail.com>
2016-04-03 12:37:21 +0530

    Location impiment

--- app/src/main/java/aitlindia/com/drishtinav/MainActivity.java
+++ app/src/main/java/aitlindia/com/drishtinav/MainActivity.java
@@ -1,14 +1,19 @@
 package aitlindia.com.drishtinav;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected static final int RESULT_SPEECH = 1;
 
     private ImageButton btnSpeak;
-    private TextView txtText;
+    private Button locationButton;
+    private TextView txtText, locationTextV;
+
+    private LocationManager locManager;
+    private LocationListener locListener;
+    private Location mobileLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         txtText = (TextView) findViewById(R.id.txtText);
+        locationTextV = (TextView)findViewById(R.id.lcoationtextView);
 
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+        locationButton = (Button)findViewById(R.id.getLocationBtn);
+
+
+
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
@@ -51,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT);
                     t.show();
                 }
+            }
+        });
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                buttonGetLocationClick();
+
             }
         });
 
@@ -101,4 +127,57 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+
+    /** Gets the current location and update the mobileLocation variable*/
+    private void getCurrentLocation() {
+        locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locListener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status,
+                                        Bundle extras) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                // TODO Auto-generated method stub
+                mobileLocation = location;
+            }
+        };
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+    }
+
+    private void buttonGetLocationClick() {
+        getCurrentLocation(); // gets the current location and update mobileLocation variable
+
+        if (mobileLocation != null) {
+            locManager.removeUpdates(locListener); // This needs to stop getting the location data and save the battery power.
+
+            String londitude = "Londitude: " + mobileLocation.getLongitude();
+            String latitude = "Latitude: " + mobileLocation.getLatitude();
+            String altitiude = "Altitiude: " + mobileLocation.getAltitude();
+            String accuracy = "Accuracy: " + mobileLocation.getAccuracy();
+            String time = "Time: " + mobileLocation.getTime();
+
+            locationTextV.setText(londitude + "\n" + latitude + "\n"
+                    + altitiude + "\n" + accuracy + "\n" + time);
+        } else {
+            locationTextV.setText("Sorry, location is not determined");
+        }
+    }
+
+
+
 }
