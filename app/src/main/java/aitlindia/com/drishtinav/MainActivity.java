f1c954964d9c9dba5a0681415a7e37cb3b21a5e5
Md Nurul Hassan <mailme.nurul@gmail.com>
2016-04-05 01:21:55 +0530

    Voice to text test to voice location is not working yet

--- app/src/main/java/aitlindia/com/drishtinav/MainActivity.java
+++ app/src/main/java/aitlindia/com/drishtinav/MainActivity.java
@@ -1,15 +1,22 @@
 package aitlindia.com.drishtinav;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,14 +26,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     protected static final int RESULT_SPEECH = 1;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private Context context;
+    private Activity activity;
+
+    private TextToSpeech engineTTS;
 
     private ImageButton btnSpeak;
-    private Button locationButton;
+    private Button locationButton, speakButton;
     private TextView txtText, locationTextV;
+    private TextToSpeech tts;
 
     private LocationManager locManager;
     private LocationListener locListener;
@@ -45,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
 
         locationButton = (Button)findViewById(R.id.getLocationBtn);
+        speakButton = (Button)findViewById(R.id.speechbutton);
+
+        engineTTS = new TextToSpeech(this, this);
 
 
 
@@ -77,12 +95,28 @@ public class MainActivity extends AppCompatActivity {
 
                 buttonGetLocationClick();
 
+
             }
         });
 
+        speakButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
 
+        });
+    }
 
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 
     @Override
@@ -128,6 +162,49 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+
+
+    private void requestPermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            Toast.makeText(context,"GPS permission allows us to access location data. Please allow in App Settings for additional functionality.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    //Snackbar.make(view,"Permission Granted, Now you can access location data.",Snackbar.LENGTH_LONG).show();
+
+                } else {
+
+                    //Snackbar.make(view,"Permission Denied, You cannot access location data.",Snackbar.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
 
 
     /** Gets the current location and update the mobileLocation variable*/
@@ -156,14 +233,42 @@ public class MainActivity extends AppCompatActivity {
                 mobileLocation = location;
             }
         };
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+
+        if (locManager != null) {
+
+            //checkPermission
+            if(android.os.Build.VERSION.SDK_INT <= 22){
+                locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+            }else{
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //locManager.removeUpdates(GPSListener.this);
+                    locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+                }
+            }
+
+        }
+
     }
 
     private void buttonGetLocationClick() {
         getCurrentLocation(); // gets the current location and update mobileLocation variable
 
         if (mobileLocation != null) {
-            locManager.removeUpdates(locListener); // This needs to stop getting the location data and save the battery power.
+
+            if (locManager != null) {
+                if(android.os.Build.VERSION.SDK_INT <= 22){
+                    locManager.removeUpdates(locListener);
+                }else{
+                    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locManager.removeUpdates(locListener);
+                        //locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+                    }
+                }
+
+            }
+             // This needs to stop getting the location data and save the battery power.
 
             String londitude = "Londitude: " + mobileLocation.getLongitude();
             String latitude = "Latitude: " + mobileLocation.getLatitude();
@@ -178,6 +283,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = engineTTS.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
+    private void speakOut() {
+
+        String text = txtText.getText().toString();
+
+        //engineTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        engineTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
 
 
 }
