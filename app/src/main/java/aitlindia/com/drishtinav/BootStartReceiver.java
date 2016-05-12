e0dc05742a58d7ca670a0049631134589c18e9f0
Md Nurul Hassan <mailme.nurul@gmail.com>
2016-05-13 01:37:16 +0530

    location update in voice continious

---
+++ app/src/main/java/aitlindia/com/drishtinav/BootStartReceiver.java
@@ -0,0 +1,22 @@
+package aitlindia.com.drishtinav;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootStartReceiver extends BroadcastReceiver {
+
+    public BootStartReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+
+
+    }
+
+
+}
